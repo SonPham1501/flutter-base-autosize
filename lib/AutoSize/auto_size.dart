d052541b472fe12e0ee9180a16806fdc6c207718
SON <son@SONs-Mac-mini.local>
2022-12-12 10:41:43 +0700

    init

---
+++ lib/AutoSize/auto_size.dart
@@ -0,0 +1,5 @@
+library auto_size;
+
+export 'src/auto_size.dart';
+export 'src/auto_size_config.dart';
+export 'src/ScreenUtil.dart';
---
+++ lib/base_autosize.dart
@@ -0,0 +1,9 @@
+library base_autosize;
+
+export './AutoSize/auto_size.dart';
+
+/// A Calculator.
+class Calculator {
+  /// Returns [value] plus 1.
+  int addOne(int value) => value + 1;
+}
